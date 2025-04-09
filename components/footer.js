dc8c33c41156f2f5ed940bc5b27da83d341924b2
Erjon Hashani <erjonndev@gmail.com>
2025-04-10 01:18:40 +0200

    Update

--- components/footer.js
+++ components/footer.js
@@ -1,7 +1,10 @@
 // Module to export the Footer component
 
 function Footer(footer_container) {
-  footer_container.innerHTML = `
+
+    const isMoviePage = window.location.pathname.includes('movie.html');
+
+    footer_container.innerHTML = `
         <div class="container mx-auto px-16 py-7">
             <div class="text-gray-200 md:flex justify-between items-center">
                 <!-- Title and info -->
@@ -21,13 +24,16 @@ function Footer(footer_container) {
                     </ul>
                 </div>
                 <!-- Shop -->
-                <div id="purchase" class="w-60 md:w-48 lg:w-auto self-baseline bg-gray-200 hover:scale-105 duration-500">
-                    <a href="cart.html" class="flex justify-center items-center gap-2 text-gray-700 py-2 px-3">Purchase now 
+                ${!isMoviePage ? `
+                    <div id="purchase" class="w-60 md:w-48 lg:w-auto self-baseline bg-gray-200 hover:scale-105 duration-500">
+                      <a href="cart.html" class="flex justify-center items-center gap-2 text-gray-700 py-2 px-3">
+                        Purchase now 
                         <svg class="size-4 fill-gray-700" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                            <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z" />
+                          <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
                         </svg>
-                    </a>
-                </div>
+                      </a>
+                    </div>
+                ` : ''}
             </div>
             <div class="border-t border-gray-600 mt-12 pt-6">
                 <p class="text-center text-gray-200">&copy; 2025 MyMovieShop. All rights reserved. Data provided by <a class="text-orange-300 hover:text-orange-600 duration-100" href="https://www.themoviedb.org/" target="_blank" rel="noopener noreferrer">TMDB</a>.</p>
